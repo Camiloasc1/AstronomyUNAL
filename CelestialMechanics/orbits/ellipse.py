a103d13acb911f8b0e835057e1a63387d578536e
caasanchezcu <caasanchezcu@unal.edu.co>
2017-03-18 19:26:21 -0500

    Orbital equations - ellipse

--- CelestialMechanics/orbits/ellipse.py
+++ CelestialMechanics/orbits/ellipse.py
@@ -93,3 +93,58 @@ def Q(a, e):
     :rtype: float
     """
     return a * (1 + e)
+
+
+def e(a: float, b: float) -> float:
+    """
+
+    :param a: semi-major axis
+    :type a: float
+    :param b: semi-minor axis
+    :type b: float
+    :return: eccentricity
+    :rtype: float
+    """
+    return np.sqrt(1 - (b * b) / (a * a))
+
+
+def c(a: float, b: float) -> float:
+    """
+    c = sqrt(a * a - b * b)
+
+    :param a: semi-major axis
+    :type a: float
+    :param b: semi-minor axis
+    :type b: float
+    :return: linear eccentricity
+    :rtype: float
+    """
+    return np.sqrt(a * a - b * b)
+
+
+def l(a: float, b: float) -> float:
+    """
+    l = b * b / a
+
+    :param a: semi-major axis
+    :type a: float
+    :param b: semi-minor axis
+    :type b: float
+    :return: semi-latus rectum
+    :rtype: float
+    """
+    return b * b / a
+
+
+def p(a: float, b: float) -> float:
+    """
+    p = b * b / c(a, b)
+
+    :param a: semi-major axis
+    :type a: float
+    :param b: semi-minor axis
+    :type b: float
+    :return: focal parameter
+    :rtype: float
+    """
+    return b * b / c(a, b)

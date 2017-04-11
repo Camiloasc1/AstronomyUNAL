a9d31f931219b28f244e50091f0e59ac7d2d7b37
caasanchezcu <caasanchezcu@unal.edu.co>
2017-04-10 20:52:34 -0500

    Orbital Determination - custom Gauss method

--- CelestialMechanics/orbital_determination/orbital_determination.py
+++ CelestialMechanics/orbital_determination/orbital_determination.py
@@ -3,8 +3,8 @@ from typing import List, Tuple
 import numpy as np
 from astropy import units as u
 
-from CelestialMechanics.orbits import ellipse, parable, hyperbola
 from CelestialMechanics import util
+from CelestialMechanics.orbits import ellipse, parable, hyperbola
 
 
 def solve(r_: List[float], r1_: List[float], mu: float, t: float) -> Tuple[
@@ -96,3 +96,51 @@ def D_rvmu(r: float, v: float, mu: float) -> float:
     :rtype: float
     """
     return r * v * v / mu
+
+
+def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float, t: float) -> Tuple[
+    float, float, float, float, float, float, float]:
+    """
+
+    :param r_1: radius vector 1
+    :type r_1: (float, float, float)
+    :param r_2: radius vector 2
+    :type r_2: (float, float, float)
+    :param r_3: radius vector 3
+    :type r_3: (float, float, float)
+    :param mu: G * (m1 + m2)
+    :type mu: float
+    :param t: time of the observation
+    :type t: float
+    :return: (a, e, i, W, w, M_r, t0)
+    :rtype: (float, float, float, float, float, float, float)
+    """
+    h_ = util.cross(r_1, r_2)
+    h = util.norm(h_)
+    h_ /= h
+
+    r1 = util.norm(r_1)
+    r2 = util.norm(r_2)
+    r3 = util.norm(r_3)
+
+    i = np.arccos(h_[2] / h)
+    W = np.arctan2(h_[0], -h_[1])
+
+    n_ = [np.cos(W), np.sin(W), 0]
+    m_ = [-np.sin(W) * np.cos(i), np.cos(W) * np.cos(i), np.sin(i)]
+
+    a = [[util.dot(n_, r_1 - r_2), util.dot(m_, r_1 - r_2)],
+         [util.dot(n_, r_1 - r_3), util.dot(m_, r_1 - r_3)]]
+    b = [r2 - r1, r3 - r1]
+    H, K = np.linalg.solve(a, b)
+
+    w = np.arctan2(K, H)
+    e = np.sqrt(H * H + K * K)
+    a = (r1 + util.dot(r_1, n_) * H + util.dot(r_1, m_) * K) / (1 - e * e)
+    angles = ellipse.angles(a, e, r1)
+    # angle = util.angle_cuadrant(angles, r_, r1_)
+    angle = angles[0]
+    E = ellipse.E_angle(e, angle)
+    M_r = ellipse.angle_M_eE(e, E)
+    t0 = ellipse.t0(M_r, t, ellipse.n(a, mu))
+    return a, e, i, W, w, M_r, t0

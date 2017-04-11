7d515fc44339861126c725cc740ef0a5eaaee49f
caasanchezcu <caasanchezcu@unal.edu.co>
2017-04-10 21:25:26 -0500

    Orbital Determination - Workshop #1, results not validated yet

---
+++ CelestialMechanics/Scripts/Workshop1.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+from astropy import units as u
+from astropy.time import Time
+from astropy.coordinates import SkyCoord
+
+from CelestialMechanics.mu import mu_sun
+from CelestialMechanics.orbital_determination.orbital_determination import solve_gauss
+
+
+def SkyCoord2xyz(c):
+    return [c.cartesian.x.to(u.au).value, c.cartesian.y.to(u.au).value, c.cartesian.z.to(u.au).value] * u.au
+
+
+c_1 = SkyCoord('02h54m23.18s', '+17d03m19.53s', distance=2.236612957 * u.au)
+c_2 = SkyCoord('03h08m30.88s', '+18d05m15.88s', distance=2.265868176 * u.au)
+c_3 = SkyCoord('03h25m34.90s', '+19d14m01.58s', distance=2.300057744 * u.au)
+
+# To x,y,z
+c_1 = SkyCoord2xyz(c_1)
+c_2 = SkyCoord2xyz(c_2)
+c_3 = SkyCoord2xyz(c_3)
+
+t1 = Time('2017-04-02T00:00:00Z', format='isot', scale='utc').jd * u.d
+t2 = Time('2017-04-07T00:00:00Z', format='isot', scale='utc').jd * u.d
+t3 = Time('2017-04-13T00:00:00Z', format='isot', scale='utc').jd * u.d
+
+print(t1, t2, t3)
+
+# http://vo.imcce.fr/webservices/miriade/?forms
+# Earth 	2017-04-02T00:00:00.00 	-0.9769221914620 	-0.2113582577129 	0.0000161609786
+# Earth 	2017-04-07T00:00:00.00 	-0.9565107703851 	-0.2948601443041 	0.0000171051404
+# Earth 	2017-04-13T00:00:00.00 	-0.9227685743776 	-0.3921175067709 	0.0000179127328
+
+earth_1 = [-0.9769221914620, -0.2113582577129, 0.0000161609786] * u.au
+earth_2 = [-0.9565107703851, -0.2948601443041, 0.0000171051404] * u.au
+earth_3 = [-0.9227685743776, -0.3921175067709, 0.0000179127328] * u.au
+
+r_1 = earth_1 + c_1
+r_2 = earth_2 + c_2
+r_3 = earth_3 + c_3
+
+a, e, i, W, w, M_r, t0 = solve_gauss(r_1, r_2, r_3, mu_sun(0), t1)
+print('a', a)
+print('e', e)
+print('i', i)
+print('W', W)
+print('w', w)
+print('M_r', M_r)
+print('t0', t0)
--- CelestialMechanics/orbital_determination/orbital_determination.py
+++ CelestialMechanics/orbital_determination/orbital_determination.py
@@ -98,7 +98,7 @@ def D_rvmu(r: float, v: float, mu: float) -> float:
     return r * v * v / mu
 
 
-def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float, t: float) -> Tuple[
+def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float, t1: float) -> Tuple[
     float, float, float, float, float, float, float]:
     """
 
@@ -110,14 +110,14 @@ def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float,
     :type r_3: (float, float, float)
     :param mu: G * (m1 + m2)
     :type mu: float
-    :param t: time of the observation
-    :type t: float
+    :param t1: time of the observation 1
+    :type t1: float
     :return: (a, e, i, W, w, M_r, t0)
     :rtype: (float, float, float, float, float, float, float)
     """
     h_ = util.cross(r_1, r_2)
+    h_ /= util.norm(h_).value
     h = util.norm(h_)
-    h_ /= h
 
     r1 = util.norm(r_1)
     r2 = util.norm(r_2)
@@ -126,15 +126,15 @@ def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float,
     i = np.arccos(h_[2] / h)
     W = np.arctan2(h_[0], -h_[1])
 
-    n_ = [np.cos(W), np.sin(W), 0]
-    m_ = [-np.sin(W) * np.cos(i), np.cos(W) * np.cos(i), np.sin(i)]
+    n_ = [np.cos(W), np.sin(W), 0] * (u.d / u.d)  # Dimensionless
+    m_ = [-np.sin(W) * np.cos(i), np.cos(W) * np.cos(i), np.sin(i)] * (u.d / u.d)  # Dimensionless
 
-    a = [[util.dot(n_, r_1 - r_2), util.dot(m_, r_1 - r_2)],
-         [util.dot(n_, r_1 - r_3), util.dot(m_, r_1 - r_3)]]
-    b = [r2 - r1, r3 - r1]
+    a = [[util.dot(n_, r_1 - r_2).to(u.au).value, util.dot(m_, r_1 - r_2).to(u.au).value],
+         [util.dot(n_, r_1 - r_3).to(u.au).value, util.dot(m_, r_1 - r_3).to(u.au).value]]
+    b = [(r2 - r1).to(u.au).value, (r3 - r1).to(u.au).value]
     H, K = np.linalg.solve(a, b)
 
-    w = np.arctan2(K, H)
+    w = np.arctan2(K, H) * u.rad
     e = np.sqrt(H * H + K * K)
     a = (r1 + util.dot(r_1, n_) * H + util.dot(r_1, m_) * K) / (1 - e * e)
     angles = ellipse.angles(a, e, r1)
@@ -142,5 +142,5 @@ def solve_gauss(r_1: List[float], r_2: List[float], r_3: List[float], mu: float,
     angle = angles[0]
     E = ellipse.E_angle(e, angle)
     M_r = ellipse.angle_M_eE(e, E)
-    t0 = ellipse.t0(M_r, t, ellipse.n(a, mu))
+    t0 = ellipse.t0(M_r, t1, ellipse.n(a, mu))
     return a, e, i, W, w, M_r, t0
